776636e20ce9cec2b872a262772697222f870744
Zach Harris <zharris505@gmail.com>
2018-01-18 22:32:23 -0700

    Adding schema to Fog and Cloud server collections. Adding host settings to Fog server

--- Design3/FogServer/collections.js
+++ Design3/FogServer/collections.js
@@ -3,3 +3,6 @@ Items = new Mongo.Collection('myitems');
 Status = new Mongo.Collection('status');
 
 Timing = new Mongo.Collection('timing');
+
+Nodes = new Mongo.Collection('nodes');
+Tags = new Mongo.Collection('tags');
