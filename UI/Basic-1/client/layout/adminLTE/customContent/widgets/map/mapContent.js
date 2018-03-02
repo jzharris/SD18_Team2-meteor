6aea1441eaac5a1a07423ad22ea681c5252af295
Chris Rey <cvrey12@gmail.com>
2018-03-01 21:09:29 -0700

    Got the legended positioned correctly.

--- UI/Basic-1/client/layout/adminLTE/customContent/widgets/map/mapContent.js
+++ UI/Basic-1/client/layout/adminLTE/customContent/widgets/map/mapContent.js
@@ -1,7 +1,7 @@
 Template.mapContent.onCreated(function() {
     // We can use the `ready` callback to interact with the map API once the map is ready.
     GoogleMaps.ready('map', function(map) {
-        // Add a marker to the map once it's ready
+
 
         var icons = {// Define Icons
             tag: {
@@ -26,21 +26,13 @@ Template.mapContent.onCreated(function() {
             }}
         };
 
-        var nodes = [];
-        var tags = [];
+        // Create arrays for node and tag pins
+        // These may need to be collections instead
+        var pins_nodes = [];
+        var pins_tags = [];
 
-        // Google Map's listener for mouse movement over the map
-        google.maps.event.addListener(map.instance, 'click', function (event) {
-                addTag(event.latLng,Math.floor(Math.random() * 60));
-        });
-
-        google.maps.event.addListener(map.instance, 'rightclick', function (event) {
-                addNode(event.latLng);
-        });
-
-        //  Trying to implement a legend
-        var legend = $(window).find('#legend')
-        console.log(legend);
+        // Draw map legend
+        map.instance.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push($('#legend')[0])
         for (var key in icons) {
           var type = icons[key];
           var name = type.name;
@@ -49,7 +41,14 @@ Template.mapContent.onCreated(function() {
           var label = '<div><svg height="22" width="22" viewBox="0 0 25 25"> <path d=' + icon + ' fill=' + color + '/></svg>' + name + '</div>';
           $('#legend').append(label)
         }
-        //map.instance.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push($(window).find('#legend'))
+
+        // Google Map's listeners for testing
+        google.maps.event.addListener(map.instance, 'click', function (event) {
+                addTag(event.latLng,Math.floor(Math.random() * 60));
+        });
+        google.maps.event.addListener(map.instance, 'rightclick', function (event) {
+                addNode(event.latLng);
+        });
 
         function displayCoordinates(latLng) {
               // Function for writing google maps coords as string
@@ -61,6 +60,8 @@ Template.mapContent.onCreated(function() {
           }
 
         function addTag(latLng,r) {
+          // Add tag marker and listeners
+          // Could set up so that tag json objects are passed instead
           var tag = {
               marker: addMarker(latLng,icons['tag'].icon),
               circle: new google.maps.Circle({
@@ -74,40 +75,41 @@ Template.mapContent.onCreated(function() {
             })
           }
 
-          tags.push(tag);
+          pins_tags.push(tag);
 
           tag.marker.addListener('rightclick', function() { // For Testing
             removeMapObject(tag.marker);
             removeMapObject(tag.circle);
-            tags.splice(tags.indexOf(tag))
+            pins_tags.splice(pins_tags.indexOf(tag))
 
           });
         }
 
         function addNode(latLng,pin_icon) {
+          // Add node marker and listeners
+          // Could set up so that node json objects are passed instead
           var node = addMarker(latLng,icons['node'].icon);
-          nodes.push(node);
+          pins_nodes.push(node);
 
           node.addListener('rightclick', function() { // For Testing
             removeMapObject(node);
-            nodes.splice(nodes.indexOf(node))
+            pins_nodes.splice(pins_nodes.indexOf(node))
           });
         }
 
         function addMarker(latLng,pin_icon) {
-            // Function for creating markers
+            // Function for creating base markers
            var marker = new google.maps.Marker({
                 position: latLng,
                 map: map.instance,
                 icon: pin_icon,
             });
-
             return marker;
         }
 
-        function removeMapObject(marker) {
+        function removeMapObject(object) {
             // Function for removing markers from the map
-            marker.setMap(null);
+            object.setMap(null);
         }
     });
 });
@@ -120,7 +122,7 @@ Template.mapContent.helpers({
             return {
                 center: new google.maps.LatLng(34.066109, -106.907439),
                 zoom: 18,
-                minZoom: 17,
+                minZoom: 16,
                 disableDefaultUI: true,
                 styles: [{
                     featureType: 'poi',
