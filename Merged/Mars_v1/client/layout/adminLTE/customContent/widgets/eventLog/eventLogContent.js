c4e33153f3de9432e8902b0301d96813cf5a0d95
Chris Rey <cvrey12@gmail.com>
2018-04-12 02:15:26 -0600

    Updated Eventlog to group node packets with collapsable lists

--- Merged/Mars_v1/client/layout/adminLTE/customContent/widgets/eventLog/eventLogContent.js
+++ Merged/Mars_v1/client/layout/adminLTE/customContent/widgets/eventLog/eventLogContent.js
@@ -3,28 +3,7 @@ Template.eventLogContent.helpers({
     return Nodes.find({});
   },
 
-  groupNodesByID: function () {
-
-    Meteor.call('groupNodesByID',function (error, result){})
+  groupedNodes: function () {
+      return SortedNodes.find({});
   },
-
-  // nodeInstances: function (node) {
-  //   console.log(Nodes.find({}));
-  //   return Nodes.find({});
-  // },
-  // icons = {
-  //     tag: {
-  //       name: 'Tag',
-  //       default: new Icon('Tag','LimeGreen'),
-  //       selected: new Icon('Tag','LightBlue'),
-  //       alert: new Icon('Tag','OrangeRed')
-  //     },
-  //     node: {
-  //       name: 'Node',
-  //       default: new Icon('Node','LimeGreen'),
-  //       selected: new Icon('Node','LightBlue'),
-  //       alert: new Icon('Node','OrangeRed')
-  //     }
-  // }
-
 });
