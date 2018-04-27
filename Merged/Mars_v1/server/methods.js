34c36e22913a467f56860d5c699aaa6b450d3608
Zach Harris <zharris505@gmail.com>
2018-04-27 00:09:04 -0600

    Adding parse to methods.js

--- Merged/Mars_v1/server/methods.js
+++ Merged/Mars_v1/server/methods.js
@@ -1,85 +1,77 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 
 Meteor.methods({
-    'resetNodes': function() {
+    'resetNodes': function () {
         Nodes.remove({});
     },
-    'resetTags': function() {
+    'resetTags': function () {
         Tags.remove({});
     },
 
     // ddp client methods:
-    'assignNode': function() {
+    'assignNode': function () {
         return assignNode();
     },
-    'arduinoStatus': function(nodeID, status) {
-        console.log('Node'+nodeID+"'s"+' arduino status changed to '+status+'!');
+    'arduinoStatus': function (nodeID, status) {
+        console.log('Node' + nodeID + "'s" + ' arduino status changed to ' + status + '!');
     },
-    'getStatus': function(){
+    'getStatus': function () {
         return Status.findOne({});
     },
-    'arduinoMessage': function(message) {
+    'arduinoMessage': function (message) {
         console.log('Message from Arduino: ');
         var result = "";
         var i;
-        for(i = 4; i < message.length; i++) {
+        for (i = 4; i < message.length; i++) {
             result += String.fromCharCode(message[i]);
         }
         console.log("\n\n" + result + "\n\n");
 
         var json = JSON.parse(result);
 
-        if(json['p']) {
-            var packet = json['p'];
-            if(packet['n'] && packet['l'] && packet['ti']) {
-              var gpstime = parseInt(packet['ti']);
-              if (typeof gpastime !== 'undefined'){
-                var t = new Date();
-                t.setHours(0,0,0,0);
-                t.setSeconds(t.getSeconds() + parseInt(packet['ti']));
-              } else {
-                var t = new Date();
-              }
+        if (json['p']) {
+            let packet = json['p'];
+            let time = null;
+            if (packet['n'] && packet['l'] && packet['ti']) {
+                let gpstime = parseInt(packet['ti']);
+                if (typeof gpstime !== 'undefined') {
+                    time = new Date();
+                    time.setHours(0, 0, 0, 0);
+                    time.setSeconds(time.getSeconds() + gpstime);
+                } else {
+                    time = new Date();
+                }
 
-                var nodeSubmission = {
+                let nodeSubmission = {
                     nodeID: packet['n'],
                     gps: {
-                        lat: packet['l']['la'],
-                        lon: packet['l']['lo'],
-                        timestamp: t
+                        lat: packet['l']['a'],
+                        lon: packet['l']['o'],
+                        timestamp: time
                     }
                 };
                 Nodes.insert(nodeSubmission);
                 console.log(nodeSubmission);
-            } else {ta
+            } else {
                 console.log('not a valid node packet');
             }
 
-            if(packet['n'] && packet['ta']) {
-                for(var t in packet['ta']) {
-                    var tagSubmission = {
-                      nodeID: packet['n'].toString(),
-                      tagID: parseInt(packet['ta'][t].I),
-                      measurements: {
-                        label: packet['ta'][t].c,
-                        data: ((packet['ta'][t].s > 0) ? parseInt(packet['ta'][t].s[0]) : 0)
-                      }
+            if (packet['n'] && packet['ta']) {
+                let split1 = packet['ta'].split(';');
+                for (let tag in split1) {
+                    if(split1[tag]) {
+                        let split2 = split1[tag].split(' ');
+                        let tagSubmission = {
+                            nodeID: packet['n'].toString(),
+                            tagID: parseInt(split2[1] ? split2[1] : 0),
+                            measurements: {
+                                label: split2[0] ? (split2[0] === 'c' ? 'cnts' : 'volt') : 'null',
+                                data: split2[2] ? parseInt(split2[2]) : []
+                            }
+                        };
+                        console.log(tagSubmission);
+                        Tags.insert(tagSubmission);
                     }
-                    console.log(tagSubmission);
-                    Tags.insert(tagSubmission);
-                    // var tagSubmission = {
-                    //     nodeID: packet['n'],
-                    //     tagID: parseInt(message[3]),
-                    //     measurements: []
-                    // };
-                    // tagSubmission.measurements.push({
-                    //     type: 'Sensor',
-                    //     label: 'Count',
-                    //     data: [{
-                    //         rssi: parseInt(packet['ta'][t].R),
-                    //         value: packet['ta'][t].s.length > 2 ? parseInt(packet['ta'][t].s[2]) : 0
-                    //     }]
-                    // });
                 }
             }
         } else {
@@ -93,7 +85,7 @@ Meteor.methods({
     //     console.log(parsed);
     // },
 
-    'downloadCSV': function(origin) {
+    'downloadCSV': function (origin) {
         const collection = collectAssets(origin);
         const heading = true;  // Optional, defaults to true
         const delimiter = ","; // Optional, defaults to ",";
@@ -101,20 +93,20 @@ Meteor.methods({
         return exportcsv.exportToCSV(collection, heading, delimiter);
     },
 
-    'randomNodeId': function(){
-      randomNode = SortedNodes.aggregate([
-        {$sample: {size: 3}},
-        {$project: {_id: 1}}
-      ]);
-      //console.log(randomNode);
-      return randomNode
+    'randomNodeId': function () {
+        randomNode = SortedNodes.aggregate([
+            {$sample: {size: 3}},
+            {$project: {_id: 1}}
+        ]);
+        //console.log(randomNode);
+        return randomNode
 
     },
 
-    'fakeNode': function(id) {
+    'fakeNode': function (id) {
         fakenode(id);
     },
-    'fakeTag': function(id) {
+    'fakeTag': function (id) {
         faketag(id);
     }
 
